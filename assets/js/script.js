3f77081982f996c2c2d526a15f6c667d9e4e9df9
Milana <msugic@gmail.com>
2021-10-04 02:06:07 +0200

    created base template for the main section, styled page heading and breadcrumbs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 console.log('Hello from js');
 
-//  Menu toggle button on mobile
+//  Menu toggle button on mobile - animate toggle btn, open and close navigation menu, body freez from scrolling
 const togglerNavbarBtn = document.querySelector('.st-toggler--navbar-btn');
 const navBarHeader = document.querySelector('.st-navbar-container');
+const body = document.querySelector('body');
 let menuOpen = false;
 
 togglerNavbarBtn.addEventListener('click', () => {
     if(!menuOpen) {
         togglerNavbarBtn.classList.add('open');
         navBarHeader.classList.add('show');
+        body.classList.add('no-scroll');
         menuOpen = true;
     } else {
         togglerNavbarBtn.classList.remove('open');
         navBarHeader.classList.remove('show');
+        body.classList.remove('no-scroll');
         menuOpen = false;
     }
 });
